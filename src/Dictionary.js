e5425de830b2a1f265a7703ce4b25700f34aaef5
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-22 11:19:15 +0100

    Added Dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    // documentation https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "ea2t80odc177b67c34d9a9706dd272ff";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {

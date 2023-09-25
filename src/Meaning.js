aac32f353d7b05bb503181ef958f10c1df4d6bdf
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-25 13:35:00 +0100

    Created new component Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <p>{props.meaning.definition}</p>
         <em>{props.meaning.example}</em>
-      </p>
-    </div>
-  );
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }

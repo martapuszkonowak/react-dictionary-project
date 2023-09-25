fd5d40f9f251515711224f3440d08d5136a5f80c
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-25 13:49:55 +0100

    Updated Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,10 @@ export default function Meaning(props) {
     return (
       <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definition}</p>
+        <p>
+          <strong>Definition:</strong> {props.meaning.definition}
+        </p>
+
         <em>{props.meaning.example}</em>
         <Synonyms synonyms={props.meaning.synonyms} />
       </div>

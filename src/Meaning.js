c7edd016b790d447f4a871593d094fff592cf62c
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-09-26 11:27:53 +0100

    set up a default word on load; styled the app

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -6,10 +7,9 @@ export default function Meaning(props) {
     return (
       <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        <p>
-          <strong>Definition:</strong> {props.meaning.definition}
-        </p>
-        <em>{props.meaning.example}</em>
+
+        <div className="definition">{props.meaning.definition}</div>
+        <div className="example">{props.meaning.example}</div>
         <Synonyms synonyms={props.meaning.synonyms} />
       </div>
     );
